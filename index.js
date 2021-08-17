8c93634ad5992dc0c72288b0432118310bb6fbfa
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-08-17 12:35:24 -0400

    feat: support alternate range-strategy for non-addons, change addon default to lockfile change

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ const { name, version } = require('./package');
 module.exports = {
   name,
   version,
+
+  locals(options) {
+    /**
+      * This blueprint is intended for addons, but it can be used with apps with
+      * minimal tweaking
+      */
+    let isAddon = options.project.isEmberCLIAddon();
+
+    return {
+      isAddon,
+    }
+  }
 };
